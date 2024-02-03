478ff3be537542173e97ee1a89df82a87b99421a
onghdzaa <68971090+onghdzaa@users.noreply.github.com>
2024-02-03 23:53:39 +0700

    Add files via upload

---
+++ src/components/Navbar-web.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+    <div class="topnav">
+        <a1 >copyright © 2023 All Rights Reserved</a1>
+        
+      <a href="#home"> <img class="icon1" src="../assets/img/google.png"> Home</a>
+      <a2 href="#contact"> <img class="icon1" src="../assets/img/google.png"> Contact</a2>
+      <!-- <a href="#"><i class="../assets/img/twitter.png"></i> Search</a>  -->
+      <img class="icon" src="../assets/img/google.png">
+    
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+
+
+
+export default {
+  data() {
+    return {  
+       isOpen:true,
+    };
+  }
+    ,
+  methods: {
+
+    }
+}
+
+</script>
+
+
+<style >
+body {margin:10;}
+.topnav {
+    width: 100%;
+  background-color: #333;
+  overflow: auto;
+
+  
+}
+.topnav a1 {
+    float: left;
+  padding: 12px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+}
+.topnav a {
+  float: left;
+  padding: 12px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+  margin-left: 40rem
+  /* display: flex; */
+    /* align-items: center;
+    justify-content: center; */
+}
+.topnav a2 {
+  float: left;
+  padding: 12px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+  margin-left: 10rem
+  /* display: flex; */
+    /* align-items: center;
+    justify-content: center; */
+}
+.icon{
+  float: right;
+  /* position: absolute; */
+    padding: 12px;
+    height: 1em;
+      /* top: 190px; */
+}
+.icon1{
+     /* margin-left: 10rem; */
+    height: 1em;
+      /* top: 190px; */
+}
+.topnav a:hover ,.topnav a2:hover{
+  background-color: #ddd;
+  color: black;
+}
+
+</style>
---
+++ src/components/Select-web.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="backimage">
+     
+    
+    </div>
+  </template>
+  <script>
+
+
+  export default {
+  
+    data() {
+        return {
+            isOpen: false,
+            active: 'register',
+            
+        };
+    },
+    methods: {
+      
+        }
+    }
+    
+
+
+  </script>
+  <style>
+  /* .btn_group{
+    
+  } */
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+ 
+  
+.backimage{
+  /* background-size: contain;
+  background-repeat: no-repeat;
+  width: 200px; */
+  height: 90vh;
+background-image:url(../assets/img/BG1.png) ;
+
+  }
+  /* Style buttons */
+.btn {
+  margin: auto;
+  top: 5%;
+  position: relative;
+    height: 2rem;
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    font-size: 1.1rem;
+    padding: 1.8rem 15rem;
+    font-weight: 600;
+    user-select: none;
+    border-radius: 4px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 10px;
+}
+
+
+  </style>
